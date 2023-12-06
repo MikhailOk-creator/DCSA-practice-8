d29bc72dc481216bc75b05ef5109d1b2e548e9ce
Mikhail Okhapkin <mikhail.okhapkin@yandex.ru>
2023-12-06 21:00:12 +0300

    Initialization of the project

---
+++ src/main/java/ru/mirea/dcsa_8/Dcsa8Application.java
@@ -0,0 +1,13 @@
+package ru.mirea.dcsa_8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dcsa8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dcsa8Application.class, args);
+    }
+
+}
