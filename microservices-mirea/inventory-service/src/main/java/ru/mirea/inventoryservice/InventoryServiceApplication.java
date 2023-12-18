57189f060e89d5ad47ad644f78711880a470c455
Mikhail Okhapkin <mikhail.okhapkin@yandex.ru>
2023-12-18 15:40:55 +0300

    Add metod for adding first products
    The method adds initial items to the inventory database. In the future, we may replace it with Flyway.

--- microservices-mirea/inventory-service/src/main/java/ru/mirea/inventoryservice/InventoryServiceApplication.java
+++ microservices-mirea/inventory-service/src/main/java/ru/mirea/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,11 @@
 package ru.mirea.inventoryservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import ru.mirea.inventoryservice.entity.Inventory;
+import ru.mirea.inventoryservice.repository.InventoryRepository;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -10,4 +14,19 @@ public class InventoryServiceApplication {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+		return args -> {
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("iphone_15");
+			inventory.setQuantity(100);
+
+			Inventory inventory1 = new Inventory();
+			inventory1.setSkuCode("iphone_15_red");
+			inventory1.setQuantity(0);
+
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventory1);
+		};
+	}
 }
